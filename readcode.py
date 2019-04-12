8e3b312865b06ed8b024258468e5c75c1c7beb6b
liujun <liujunnuist@163.com>
2019-04-12 19:25:21 +0800

    0412

--- readcode.py
+++ readcode.py
@@ -1,67 +1,128 @@
 一. 加载数据 #load_data.py
     1.  pi : Product information
         获取的是昨天的数据
-        sku级别的去重复
-        滤除所有包含NaN 、 转换数据类型、排序sort_values、设置index
+        sku级别的去重复、滤除所有包含NaN 、 转换数据类型、排序sort_values、设置index
 
     2.  si : Store basic information
         选取正常营业的门店
         mng_reg_id = 大区id (dq_id) + 地区id (zone_id) 组合
+        org级别的去重复、滤除所有包含NaN 、 转换数据类型、排序sort_values、设置index
 
     3.  wi : Warehouse basic information	仓库基本信息
-    4.  ms : Main sizes
-    5.  i0 : Initial inventory data      期初存货
-    6.  s  : Sales data 
-    7.  mv : Moving data  （组织的发送方、接收方、日期、数量等）
-    8.  mr : Moving restriction states  （not_in, not_out 门店因为装修等原因不能进出）
-    9.  mp :  Store moving period  （index : org_id	columns : is_rep, is_trans ？）
+             index : org_id     
+             columns : prov_id, city_id
+        如果为空，则设置为浙江 宁波 #WHERE store_id = '420867' ？？？？
 
-二. Pre-processing data  预处理数据
+    # 4.  ms : Main sizes
+    4.  i0 : Initial inventory data      期初存货
+             index : prod_id, color_id, size, org_id
+             columns : i0, r
+
+             i0 : 期初库存，前一天的库存量
+             r  ：在途库存，期待到达的日期在9天前 到7天前，#####？？？？？？？
+                 sum（期待到达的数量 - 实际到达的数量）
+            'prod_id', 'color_id', 'size', 'org_id'级别去重复、滤除所有包含NaN 、 转换数据类型、排序sort_values、设置index
+
+    5.  s  : Sales data 
+            index : prod_id, color_id, size, org_id
+            columns : date_sell, s
+
+            s: 销量qty之和  ##？？为啥是是一个月 between 1 and 30
+
+    6.  mv : Moving data  
+         index : prod_id, color_id, size
+         columns : org_send_id, org_rec_id, date_send, date_rec, quant_send (发送数量),
+                   quant_rec (接受数量)
+        6.1 从库存移动事实表中，选取发送日期 三个月内的数据 #三个月内？？
+        6.2 删除 quant_send < 0 的数据
+        6.2 删除NA, 转化数据格式，排序，设置index
+
+    7.  mr : Moving restriction states 
+            index : prod_id, color_id, org_id
+            columns : not_in, not_out 
+           （not_in, not_out 门店因为装修等原因不能进出）
+            选取当前日期 正常营业的门店的移动现实状态（具体到某个门店的skc）
+
+    8.  mp :  Store moving period   ##？？？
+              index : org_id	
+              columns : is_rep, is_trans 
+              选取当前日期的正常营业的门店的数据 
+              is_rep, is_trans : (0, 1)变量, Y 为1，否则为0 
+
+######################################################################
+二. 预处理数据 #prep_data.py
     pi, si, po, i0, s, qsf, io = prp.prep_data(pi, si, wi, ms, i0, s, mv, mr, mp, pl.date_dec, pl.qsf_base,pl.prt_per_in)
+    
     return pi_p, si_p, po, i0_f, s_f, qsf, io
 
     date_dec : Decision making date (date)
     qsf_base : Basic continue-size length in a fullsize group (int)
     prt_per_in : Moving in protecting period (int)
 
-    1.期初库存(i0)
-        1）将skc或org初始库存之和为0， 设置skc或org的sku的库存 都为0  (为什么要有置零操作？)
-        2）将所有为负数的库存都置为0
-    2. 统计每周总销售额（s, date_dec） Aggregating weekly sales 
-        1) 按7天 销售数据分组
+    1. 处理期初库存(i0) #i0_p = proc_neg_inv(i0)
+        1）如果每个org的skc期初库存之和为0， 设置sku的期初库存 置为0  ###为什么要有置零操作？？？
+        2）设置 所有期初库存i0 <0 置为0
+        3) 设置 所有在途库存r <0 置为0
+        4) 进行 第 6 步
+
+    2. 统计每周总销售额 #s_week = agg_week_sales(s, date_dec)
+        s_week : pd.DataFrame
+             Weekly sales
+             index : prod_id, color_id, size, org_id, week_no
+             columns : s
+        1) 按7天 销售数据分组， -1 代表上周，-2 代表上上周
         2）分组求和，计算每周的销量
-        3）将销量<0,置零
-    3. 处理产品信息(pi, i0, mr)
-        1）计算sku的最大期初库存max，max大于0的才认为有效
-        2）计算每个skc的移动限制，选择能够移动的
+        3）将周销量s <0, 置零
+        
+    3. 处理产品信息 #pi_p = proc_prod_info(pi, i0_p, mr)
+        1）计算sku的最大期初库存i0_max，max大于0的才认为有效 ###代码
+        2）计算每个skc的移动限制，选择同时能够移入移出的sku
+            not_in * not_out >0  => not_in >1 and not_out >1  ####?????
         3）选择满足1）和2）的商品
-    4. 处理门店信息(si, i0, s, mr, mp)
-        1）计算每个组织的累加期初库存，选出 大于0的组织
-        2）计算每个组织的上一周(week_no == -1)的销量总和, 选出 大于0的组织
-        3）选择 满足两者之一 (与或条件)的组织
-        4）计算每个门店的移动状态，选择能够进出的门店
-        5）选择满足3）4）的组织
-    5. 交叉商品和门店信息 (pi, si, wi, i0, s, mr)
-        1) 门店信息(is_store=0) 与 仓库信息（is_store=1）
-        2) 合并数据 ？
-        3）选择skc和组织 满足 期初库存 或 前两周最大销量 大于 0 的
-    6. 填充 5）得到的表中的无效的数据 (pi_p, si_p, wi, i0_p, s_week, mr)
+
+    4. 处理门店信息 #si_p = proc_org_info(si, i0_p, s_week, mr, mp)
+        1）计算每个门店的累加库存i0_sum （i0_sum = 期初库存i0 + 在途库存 r），选出 i0_sum > 0的门店
+        2）计算每个门店的上一周(week_no == -1)的销量总和, 选出 销量 > 0 的门店
+        3）选择 满足1) 或 2) 两者之一 (与或条件)的门店
+        4）计算每个门店的移动限制，选择同时能够移入移出的门店
+        5）选择满足3）不满足4）的组织  ## ~~~~？？？
+
+    5. 商品和门店信息 #po = cross_prod_org(pi_p, si_p, wi, i0_p, s_week, mr)
+        po : pd.DataFrame
+            Crossed products and organizations
+            index : prod_id, color_id, size, org_id
+            columns : size_order, year, season_id, class_0, prov_id, city_id,
+                   dist_id, mng_reg_id, is_store, is_rep, is_trans
+
+        1) 设置门店信息(is_store=1) 与 仓库信息（is_store=0）标记
+        2) 合并数据
+            将门店信息 与 RDC信息合并；再与产品信息合并
+            选取 前两周的销售数据，
+        3）选择skc和组织 满足 期初库存 或 前两周最大销量(期初库存i0+ 在途库存r+销量s）> 0 的 skc和组织对
+            is_store != 1
+            ###代码？？？
+
+    6. 填充 5) 得到的表中的无效的数据 (pi_p, si_p, wi, i0_p, s_week, mr)
         1）将5的数据表 去重后 join 初始库存表
         2）填充NaN 为 0
+
     7) 填充销售数据 (po, i0)
         1）产品组织表去重
         2) 生成周表。week_no = [-4, -3, -2, -1]
         3) 合并 生成完整的每周销售数据
+
     8) 计算最小的连码数 (ms, qsf_base)
         1) 设置基本的连码数
         2）对于大于基本连码数的 设置 = 基本连码数（qsf_base）
+
     9) 标记一种内是否有移入移出 (po, mv, date_dec, prt_per_in)
         1) 七天的保护周期
         2）计算当前时间的一周内 累积的发出量 之和
         3）计算当前时间的一周内 累计的接收量 之和
         4）计算当前时间的一周内 发出与接收之和
-        5）去重 将NA置0， 如果有库存 >0 has_in=1, else has_in= 0
+        5）去重、NA置0， 如果有库存 >0 has_in=1, else has_in= 0
 
+######################################################################
 三. 获取权重 extr_we.py  
     sales_we, sr_we, ib_we = etw.extr_we(po, si, wi, ms, i0, s)
 
@@ -97,6 +158,7 @@
         3.2 合并数据并计算各规模初始库存和销售额之和的相关系数。
         3.3 计算库存平衡权重
 
+######################################################################
 四、计算成本参数 cal_cost_params.py
     cmq, cmp, cid, cdl, cbs = \
     ccp.cal_cost_params(pi, si, wi, po, io, sales_we, sr_we, ib_we,
@@ -130,7 +192,7 @@
         5.2 产品信息表 与 库存平衡权重表 合并，NA用最大值清洗
         5.3 用权重乘以单位断码率的成本
 
-####################
+######################################################################
 五、计算目标库存 cal_targ_inv.py
     d, qss, it = cti.cal_targ_inv(po, ms, i0, s, sales_we, cdl, cid, pl.w,
                               pl.qss_base)
@@ -232,13 +294,13 @@
         it = adj_targ_inv(po, it)
         1. 合并目标库存表 与 产品组织表，并置NA为0
 
-####################
+######################################################################
 六、初始化数据 #postp_data.py
     q, i0_aft_mov, io_aft_mov = psp.init_data(i0, io)
     1. 组织之间每个sks的移动数量 q (空表)
     2. 更新 初始库存（复制）i0_aft_mov 和 移动/移出 （hash_in = 0）
 
-####################
+######################################################################
 七、提取补货的目标skc/org #extr_dec_targ.py
     po_rep = edt.extr_po_rep(po, i0_aft_mov, mr, it)
     1. 合并数据
@@ -246,7 +308,7 @@
     3. 对于每个skc, 组织 必须是仓库，且 期初库存 > 0
     4. 求和  移入/移出的标记
 
-####################
+######################################################################
 八、执行补货 #mov_opt.py
     q_rep, cmp = mvp.exec_rep(po_rep, i0_aft_mov, it, cmq, cmp, cid)
     exec_rep(po, i0, it, cmq, cmp, cid)
@@ -286,7 +348,7 @@
             将移动数量累加求和，如果大于0，则将包裹的cmp置0 （因为是分段算的，第一段已经计算后，第二段应该将单位成本置0）
 
 
-####################
+######################################################################
 九、更新 #postp_data.py
     psp.postp_data(q, q_rep, i0_aft_mov, io_aft_mov)
     1. 更新移动数量
@@ -313,7 +375,7 @@
         如果移入数量 > 0, has_in =1
         如果移出数量 > 0, has_out =2
 
-####################
+######################################################################
 十、执行skc/org调拨 # extr_dec_targ.py
     edt.extr_po_trans(po, i0_aft_mov, io_aft_mov, mr, it)
     extr_po_trans(po, i0, io, mr, it)
@@ -341,7 +403,7 @@
     4. 挑选 to_out_skcr > 0 且 to_mov_skcs 的数据
 
 
-####################
+######################################################################
 十一、执行调拨优化 # mov_opt.py
     q_trans, cmp = mvp.exec_trans(po_trans, ms, i0_aft_mov, it, qsf, cmq, cmp, cid,
                               cbs)
@@ -412,12 +474,12 @@
             3）移入 和 移出 不能同时发生 (如果不这样算，多算了一边变量)
                 qib + qob <= 1
 
-####################
+######################################################################
 十二、 更新
     再次执行 第九步，更新所有的数据
     q, i0_aft_mov, io_aft_mov = psp.postp_data(q, q_trans, i0_aft_mov, io_aft_mov)
 
-####################
+######################################################################
 十三、 统计
     输出信息：版本、更新、决策时间、运行时间
     补货：
